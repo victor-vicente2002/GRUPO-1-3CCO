f872bbfe63092816d61ba7c4497e8d32553df83b
victor-vicente2002 <victor.casimiro@bandtec.com.br>
2022-03-11 22:28:19 -0300

    Changes in python script to add data in new database

--- Shared/Vicente/projeto_algas.py
+++ Shared/Vicente/projeto_algas.py
@@ -13,7 +13,8 @@ def sum_of_init(n):
         accumulate += i
     # mv = memoryview(b'accumulate')
     dt_fim = time.time()
-    state.append({'i': i, 'accumulate': accumulate, 'memory': sys.getsizeof(accumulate), 'time': (dt_fim - dt_inicio)})
+    memory = tracemalloc.get_traced_memory()[1]
+    state.append({'i': i, 'transactions': n, 'accumulate': accumulate, 'memory': memory, 'time_elapsed': (dt_fim - dt_inicio)})
     tracemalloc.stop()
     print(state)
     return state
@@ -27,7 +28,7 @@ def connectAndInsert(insertedValues):
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(
           host="localhost",
-          database="velocimetro",
+          database="measures",
           user="postgres",
           password="senha123",
           port="3306")
@@ -35,8 +36,8 @@ def connectAndInsert(insertedValues):
         cur = conn.cursor()
         for i in insertedValues:
 	      # execute a statement
-            postgres_insert_query = """ INSERT INTO insert_values (accumulate, memory, time) VALUES (%s,%s,%s)"""
-            record_to_insert = (i['accumulate'], i['memory'], i['time'])
+            postgres_insert_query = """ INSERT INTO measures (transactions, accumulate, memory, time_elapsed) VALUES (%s,%s,%s,%s)"""
+            record_to_insert = (i['transactions'], i['accumulate'], i['memory'], i['time_elapsed'])
             print('PostgreSQL database version:')
             cur.execute(postgres_insert_query, record_to_insert)
             conn.commit()
@@ -48,15 +49,15 @@ def connectAndInsert(insertedValues):
 	# close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        print(f'Erro: {error}')
     finally:
         if conn is not None:
             conn.close()
             print('Database connection closed.')
 
-# for valor in [100000, 2000000, 9000000, 7500000, 1000000, 24449, 9760978, 10, 78, 1325769]:
-#     # iteracoes = sum_of_init(valor)
-#     connectAndInsert(sum_of_init(valor))
+for valor in [100000, 2000000, 9000000, 7500000, 1000000, 24449, 9760978, 10, 78, 1325769, 100000]:
+    # iteracoes = sum_of_init(valor)
+    connectAndInsert(sum_of_init(valor))
 
-for valor in range(1, 10 ** 5):
-    sum_of_init(valor)
+# for i in range(1, 1000000000000):
+#     sum_of_init(i)
