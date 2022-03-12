43c7465a13146693f99ae2b7c220bc33c9bb33ae
victor-vicente2002 <victor.casimiro@bandtec.com.br>
2022-03-11 21:14:14 -0300

    Fixing field

--- Shared/Vicente/script.sql
+++ Shared/Vicente/script.sql
@@ -5,7 +5,7 @@ CREATE DATABASE measures;
 CREATE TABLE measures(
 id serial PRIMARY KEY,
 transactions numeric,
-accumulate double precision,
+accumulate numeric,
 memory numeric,
 time_elapsed numeric(12,10),
 exec_date timestamp default now());
