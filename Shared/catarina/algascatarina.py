8986e8cd3d3c61795e4134b62e00f05069c18f8e
Catarina M. Carneiro <61799757+Catarina-Carneiro@users.noreply.github.com>
2022-03-12 23:36:59 -0300

    Versão atualizada com mais ranges

--- Shared/catarina/algascatarina.py
+++ Shared/catarina/algascatarina.py
@@ -16,18 +16,19 @@ if con.is_connected():
 
 def algas(bank_transaciton):
     tracemalloc.start()
-
+    
     dt_inicio = datetime.now()
     dt_fim = 0
     accumulate= 0
-
+    
     for i in range(1, bank_transaciton+1):
         accumulate += i
     dt_fim = datetime.now()
 
     time_elapsed = dt_fim - dt_inicio
 
-    dadosReturn = f" {bank_transaciton},{accumulate},{tracemalloc.get_traced_memory()[1]},{time_elapsed.total_seconds()} "
+    
+    dadosReturn = f" {bank_transaciton},{accumulate},{tracemalloc.get_traced_memory()[1] / 10},{time_elapsed.total_seconds()} "
 
     tracemalloc.stop()
 
@@ -35,7 +36,7 @@ def algas(bank_transaciton):
 
 
 def insertDados(paramentosData):
-
+     
     mycursor = con.cursor()
 
     mycursor.execute(f'''INSERT INTO insert_values (bank_transaciton, accumulate, memory, time_elapsed) 
@@ -50,3 +51,38 @@ for valor in range(100000, 600000, 100000):
     algasDados = algas(valor)
     valores.append(algasDados)
     insertDados(algasDados)
+
+# b)
+    valores = []
+for valor in range(1000, 6000, 100):
+    algasDados = algas(valor)
+    valores.append(algasDados)
+    insertDados(algasDados)
+    
+
+# c)
+    valores = []
+for valor in range(100, 600, 100):
+    algasDados = algas(valor)
+    valores.append(algasDados)
+    insertDados(algasDados)
+
+
+# d)
+    valores = []
+for valor in range(10, 60, 10):
+    algasDados = algas(valor)
+    valores.append(algasDados)
+    insertDados(algasDados)
+    
+
+# e)
+    valores = []
+for valor in range(1000000, 6000000, 1000000):
+    algasDados = algas(valor)
+    valores.append(algasDados)
+    insertDados(algasDados)
+
+cursor.close()
+con.close()
+print("Conexao encerrada ",linha)
