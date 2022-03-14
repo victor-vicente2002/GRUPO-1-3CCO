b2ee7726e3e166a2ea94e0eae621eb2c2b5799f8
andre-moura <61800450+andre-moura@users.noreply.github.com>
2022-03-14 19:29:43 -0300

    projeto algas

---
+++ Shared/andre/sprint_create_database.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bd_memory_time;
+CREATE DATABASE bd_memory_time;
+USE bd_memory_time;
+CREATE TABLE tbl_memory_time(
+	id INT PRIMARY KEY auto_increment,
+    byte_memory INT,
+    runtime DECIMAL(18,2),
+    acumulator LONG,
+    iterator_number INT
+)auto_increment = 1;
+
+# TRUNCATE tbl_memory_time;
+# SELECT * FROM tbl_memory_time;
